345140030b6a2c07d3ad9fa9b0bbf5e6943baae7
David Gelpi <gelpiorama@gmail.com>
2024-02-02 00:21:29 +0100

    feat(base de datos): Describir los rasgos de todos los personajes del juego

--- src/quienesquien.pl
+++ src/quienesquien.pl
@@ -33,17 +33,54 @@ escribirPersonaje(Nombre):-
 
 % BBDD para representar personajes y sus características físicas mediante estructuras
 
-personaje(herman / [hombre, pelirrojo, calva, nariz_grande, ojos_marrones]).
+personaje(herman / [hombre, pelirrojo, calva, nariz_grande, ojos_marrones, cejas_gruesas]).
 
-personaje(maria / [pelo_largo, sombrero, pendientes, castaño, mujer, ojos_marrones, boca_pequeña]).
+personaje(maria / [mujer, pelo_largo, sombrero, pendientes, pelo_castaño, ojos_marrones, boca_pequeña, cejas_finas]).
 
-personaje(claire / [gafas, sombrero, mujer, pelirrojo, ojos_marrones, boca_pequeña]).
+personaje(claire / [mujer, gafas, sombrero, pelirrojo, ojos_marrones, boca_pequeña]).
 
-personaje(alfred / [hombre, pelo_largo, bigote, pelirrojo, ojos_marrones, boca_pequeña]).
+personaje(charles / [hombre, bigote, pelo_rubio, ojos_marrones, labios_gruesos, boca_grande, orejas_grandes, raya_al_lado]).
 
-personaje(charles / [hombre, bigote, rubio, ojos_marrones, labios_gruesos, boca_grande, orejas_grandes]).
+personaje(richard / [hombre, calva, barba, ojos_marrones, orejas_grandes, bigote]).
 
-personaje(richard / [calva, barba, ojos_marrones, orejas_grandes, bigote, hombre]).
+personaje(eric / [hombre, pelo_rubio, gorra, sombrero, ojos_marrones, boca_grande]).
+
+personaje(alex / [hombre, bigote, pelo_negro, ojos_marrones, boca_grande, labios_gruesos, orejas_grandes, pelo_corto]).
+
+personaje(peter / [hombre, canas, pelo_blanco, nariz_grande, ojos_azules, cejas_gruesas, labios_gruesos, boca_grande, raya_al_lado]).
+
+personaje(philip / [hombre, barba, pelo_negro, ojos_marrones, orejas_grandes, mofletes, mejillas_sonrosadas, cejas_finas, pelo_corto]).
+
+personaje(joe / [hombre, gafas, pelo_rubio, ojos_marrones, boca_pequeña, pelo_corto]).
+
+personaje(paul / [hombre, gafas, pelo_blanco, canas, ojos_marrones, boca_pequeña, orejas_grandes, cejas_gruesas, raya_al_lado]).
+
+personaje(david / [hombre, barba, pelo_rubio, ojos_marrones, orejas_grandes, raya_al_lado]).
+
+personaje(george / [hombre, cara_triste, sombrero, pelo_blanco, canas, ojos_marrones, boca_grande, nariz_pequeña]).
+
+personaje(frans / [hombre, pelo_corto, cejas_gruesas, pelirrojo, ojos_marrones, boca_pequeña]).
+
+personaje(alfred / [hombre, bigote, barba, pelirrojo, ojos_marrones, boca_pequeña, orejas_grandes, pelo_largo, raya_al_medio]).
+
+personaje(bernard / [hombre, pelo_castaño, sombrero, ojos_marrones, boca_pequeña, cejas_finas]).
+
+personaje(bill / [hombre, barba, pelirrojo, ojos_marrones, orejas_grandes, mofletes, mejillas_sonrosadas, calva, boca_pequeña, nariz_pequeña]).
+
+personaje(anita / [mujer, pelo_largo, pelo_rubio, ojos_marrones, boca_pequeña, mofletes, mejillas_sonrosadas, raya_al_medio, nariz_pequeña]).
+
+personaje(robert / [hombre, cara_triste, pelo_castaño, ojos_azules, orejas_grandes, nariz_grande, raya_al_lado]).
+
+personaje(anne / [mujer, pelo_corto, pendientes, pelo_negro, ojos_marrones, boca_pequeña]).
+
+personaje(sam / [hombre, gafas, calva, pelo_blanco, canas, boca_pequeña]).
+
+personaje(tom / [hombre, gafas, calva, pelo_negro, boca_pequeña]).
+
+personaje(susan / [mujer, pelo_largo, pelo_blanco, canas, ojos_marrones, labios_gruesos, mofletes, mejillas_sonrosadas, nariz_pequeña, raya_al_lado]).
+
+personaje(max / [hombre, bigote, pelo_negro, ojos_marrones, boca_grande, labios_gruesos, nariz_grande, orejas_grandes, pelo_corto]).
+
 
 % main
 
