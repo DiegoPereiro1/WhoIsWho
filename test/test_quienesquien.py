1f3f847ef0116f9f6832a6463bd000a0518e5d26
David Gelpi <gelpiorama@gmail.com>
2024-02-05 12:16:14 +0100

    test(test_quienesquien): Definir la relacion tiene(personaje, rasgo)

--- test/test_quienesquien.py
+++ test/test_quienesquien.py
@@ -6,16 +6,20 @@ prolog.consult('src/quienesquien.pl')
 
 @pytest.mark.levantar
 def test_levantar_tablero():
-    query = list(prolog.query("levantar_tablero(Tablero)"))
+    '''
+    ?- levantar_tablero(Tablero).
+    Tablero = [max, susan, tom, sam, anne, robert, anita, bill, bernard|...].
+    '''
+    query = list(prolog.query("levantar_tablero(Tablero)."))
     # devuelve una lista de diccionarios,
     # clave variable, valor propuestas de prolog
     # print(pregunta)
-    '''
-    [{'Tablero': ['max', 'susan', 'tom', 'sam', 'anne', 'robert', 'anita', 
-    'bill', 'bernard', 'alfred', 'frans', 'george', 'david', 'paul', 'joe', 
-    'philip', 'peter', 'alex', 'eric', 'richard', 'charles', 'claire', 'maria', 
-    'herman']}]
-    '''
+    #
+    # [{'Tablero': ['max', 'susan', 'tom', 'sam', 'anne', 'robert', 'anita', 
+    # bill', 'bernard', 'alfred', 'frans', 'george', 'david', 'paul', 'joe', 
+    # philip', 'peter', 'alex', 'eric', 'richard', 'charles', 'claire', 'maria', 
+    # herman']}]
+
     tablero = query[0]['Tablero']
     assert tablero == ['max', 'susan', 'tom', 'sam', 'anne', 'robert', 
                         'anita', 'bill', 'bernard', 'alfred', 'frans', 
@@ -25,6 +29,18 @@ def test_levantar_tablero():
 
 @pytest.mark.bbdd
 def test_personaje():
-    query = list(prolog.query("personaje(tom /_)"))
-    respuesta = query[0]
-    assert not respuesta
+    '''
+    ?- personaje(tom /_).
+    false.
+    '''
+    query = list(prolog.query("personaje(tom)."))
+    assert not query
+
+@pytest.mark.tiene
+def test_tiene():
+    '''
+    ?- tiene(maria, sombrero).
+    true ;
+    '''
+    query = bool(list(prolog.query("tiene(maria, sombrero).")))
+    assert query
