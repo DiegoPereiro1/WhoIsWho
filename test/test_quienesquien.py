a5a3bfc715b321df808c37e504ffe1e7f8863fa3
David Gelpi <gelpiorama@gmail.com>
2024-02-05 10:13:55 +0100

    test(test_quienesquien): Definir test consulta personaje

--- test/test_quienesquien.py
+++ test/test_quienesquien.py
@@ -6,19 +6,25 @@ prolog.consult('src/quienesquien.pl')
 
 @pytest.mark.levantar
 def test_levantar_tablero():
-    pregunta = list(prolog.query("levantar_tablero(Tablero)"))
+    query = list(prolog.query("levantar_tablero(Tablero)"))
     # devuelve una lista de diccionarios,
     # clave variable, valor propuestas de prolog
-    print(pregunta)
+    # print(pregunta)
     '''
     [{'Tablero': ['max', 'susan', 'tom', 'sam', 'anne', 'robert', 'anita', 
     'bill', 'bernard', 'alfred', 'frans', 'george', 'david', 'paul', 'joe', 
     'philip', 'peter', 'alex', 'eric', 'richard', 'charles', 'claire', 'maria', 
     'herman']}]
     '''
-    tablero = pregunta[0]['Tablero']
-    assert  tablero == ['max', 'susan', 'tom', 'sam', 'anne', 'robert', 
+    tablero = query[0]['Tablero']
+    assert tablero == ['max', 'susan', 'tom', 'sam', 'anne', 'robert', 
                         'anita', 'bill', 'bernard', 'alfred', 'frans', 
                         'george', 'david', 'paul', 'joe', 'philip', 
                         'peter', 'alex', 'eric', 'richard', 'charles', 
                         'claire', 'maria', 'herman']
+
+@pytest.mark.bbdd
+def test_personaje():
+    query = list(prolog.query("personaje(tom /_)"))
+    respuesta = query[0]
+    assert not respuesta
