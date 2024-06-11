67865aa9672785ed77fc2903d17ad00173754ae5
Diego Pereiro <diegopereiromartinezsayanes@gmail.com>
2024-06-11 16:49:26 +0200

    Programa mejorado

--- src/BaseDeDatos.pl
+++ src/BaseDeDatos.pl
@@ -2,3 +2,5 @@ personaje(alex, [hombre, gafas, sombrero, bigote]).
 personaje(maria, [mujer, gafas, sombrero, rubia]).
 personaje(pedro, [hombre, barba, calvo]).
 personaje(ana, [mujer, pelirroja, gafas]).
+personaje(luis, [hombre, barba, gafas, sombrero]).
+personaje(sara, [mujer, pelirroja, sombrero]).
